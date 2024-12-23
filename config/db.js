29e58cbb9d226b4f87fab4a175881de9fe0d05f4
umer321-apex <mu0829332@gmail.com>
2024-12-23 23:22:02 +0500

    mongo uri added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config(); // Load .env variables
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/miniSocialFeed", {
+    // await mongoose.connect("mongodb+srv://mu0829332:8SNmjqED4Q1TRaJv@cluster0.bt7ub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    await mongoose.connect(process.env.MONGO_URI, { // Use environment variable
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- server.js
+++ server.js
@@ -62,9 +62,7 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname,  "frontend", "build", "index.html")); // Corrected path
 });
-app.get("/", (req, res) => {
-  res.send("Hi from backend");
-});
+
 
 // Socket.IO Connection
 io.on("connection", (socket) => {
