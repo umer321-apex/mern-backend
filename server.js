6d29a8baa3974aab0bbd21976a33e9c4548b902e
umer321-apex <mu0829332@gmail.com>
2024-12-23 21:32:11 +0500

    message added

--- server.js
+++ server.js
@@ -58,6 +58,9 @@ app.use("/api/videos", require("./routes/videoRoutes"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/", (req, res) => {
+  res.send("Hi from backend");
+});
 
 // Socket.IO Connection
 io.on("connection", (socket) => {
