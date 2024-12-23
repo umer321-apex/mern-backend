2e78cde18ae604b50a878e2b5e7f93a982442cfa
umer321-apex <mu0829332@gmail.com>
2024-12-23 22:58:54 +0500

    buikd added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,59 +2,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
-// exports.register = async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     console.log("Received data:", req.body); // Log the incoming data
-//     if (!username || !password) {
-//       return res.status(400).json({ error: "Username and password are required" });
-//     }
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) {
-//       return res.status(409).json({ message: "User already registered" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = new User({ username, password: hashedPassword });
-//     await user.save();
-//     res.status(201).json({ message: "User registered successfully" });
-//   } catch (error) {
-//     res.status(400).json({ error: "Registration failed" });
-//   }
-// };
-
-
-// exports.register = async (req, res) => {
-//   try {
-//     const { username, password ,email} = req.body;
-//     console.log("Received data:", req.body); // Log the incoming data
-
-//     if (!username || !email || !password) {
-//       return res.status(400).json({ error: "Username, email, and password are required" });
-//     }
-//     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//     if (!emailRegex.test(email)) {
-//       return res.status(400).json({ error: "Invalid email address" });
-//     }
-//     const existingEmail = await User.findOne({ email });
-//     if (existingEmail) {
-//       return res.status(400).json({ error: "Email is already in use" });
-//     }
-
-//     // Check if the user already exists
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) {
-//       return res.status(400).json({ error: "User already registered" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = new User({ username, password: hashedPassword });
-//     await user.save();
-//     res.status(201).json({ message: "User registered successfully" });
-//   } catch (error) {
-//     res.status(400).json({ error: "Registration failed" });
-//   }
-// };
 
 exports.register = async (req, res) => {
   try {
@@ -116,25 +63,14 @@ exports.register = async (req, res) => {
 };
 
 
-// exports.login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-//     if (!user || !(await bcrypt.compare(password, user.password))) {
-//       return res.status(401).json({ error: "Invalid credentials" });
-//     }
-//     const token = jwt.sign({ userId: user._id }, "secretkey", { expiresIn: "1h" });
-//     res.json({ token, message: "User successfully logged in" });
-   
-//   } catch (error) {
-//     res.status(400).json({ error: "Login failed" });
-//   }
-// };
+
 
 
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log('email',email);
+    console.log('password',password);
     const user = await User.findOne({ email });
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ error: "Invalid credentials" });
--- server.js
+++ server.js
@@ -53,10 +53,14 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use(express.static(path.resolve("./public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static("./frontend/build")); 
 app.use("/api/videos", require("./routes/videoRoutes"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname,  "frontend", "build", "index.html")); // Corrected path
 });
 app.get("/", (req, res) => {
   res.send("Hi from backend");
